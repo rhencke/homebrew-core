0ebcb37585654c2bbf62524a3d8150f0b830b073
Robert Hencke <robert.hencke@kronos.com>
2016-06-14 12:04:31 -0400

    gradle 3.0-milestone-1 (devel)

--- Formula/gradle.rb
+++ Formula/gradle.rb
@@ -5,9 +5,9 @@ class Gradle < Formula
   sha256 "0f665ec6a5a67865faf7ba0d825afb19c26705ea0597cec80dd191b0f2cbb664"
 
   devel do
-    url "https://downloads.gradle.org/distributions/gradle-2.14-rc-5-bin.zip"
-    sha256 "dcdd1021345cfabd2c06a345700afe537bde5478cfb8c3ac59d6348eeb0647e9"
-    version "2.14-rc-5"
+    url "http://services.gradle.org/distributions/gradle-3.0-milestone-1-bin.zip"
+    sha256 "dfff9f7fbf75d025cf5b4f7b61ea9e24757f74a6934ba7249658e7e2de9d329f"
+    version "3.0-milestone-1"
   end
 
   bottle :unneeded
